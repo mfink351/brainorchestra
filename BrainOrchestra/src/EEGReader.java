7f0ed0e4312e611dced2fe8a89f263f774f12e88
unknown <m.fink351@gmail.com>
2015-07-16 11:57:55 -0400

    Added ability to write the read EEG waves to file

--- BrainOrchestra/src/EEGReader.java
+++ BrainOrchestra/src/EEGReader.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -33,94 +36,10 @@ public class EEGReader {
 		int i = rn.nextInt(max - min + 1) + min;
 		
 		
+		ArrayList<Double> readings = this.getReadings();
 		
 		
-		
-		
-		/**
-		 * EEG TEST CODE
-		 */
-		
-		ArrayList<Double> attention = new ArrayList<Double>();
-		ArrayList<Double> meditation = new ArrayList<Double>();
-		ArrayList<Double> delta = new ArrayList<Double>();
-		ArrayList<Double> theta = new ArrayList<Double>();
-		ArrayList<Double> alpha1 = new ArrayList<Double>();
-		ArrayList<Double> alpha2 = new ArrayList<Double>();
-		ArrayList<Double> beta1 = new ArrayList<Double>();
-		ArrayList<Double> beta2 = new ArrayList<Double>();
-		ArrayList<Double> gamma1 = new ArrayList<Double>();
-		ArrayList<Double> gamma2 = new ArrayList<Double>();
-		
-		long startTime = System.currentTimeMillis();
-		int packetsRead = 0;
-		while(getSeconds(System.currentTimeMillis(), startTime) < .5){
-			do{
-				packetsRead = ThinkGear.ReadPackets(connectionId, -1);
-				
-				//Read in all waves
-				
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_ATTENTION) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_ATTENTION);
-					System.out.println("Get attention data: " + temp);
-					attention.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_MEDITATION) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_MEDITATION);
-					System.out.println("Get meditation data: " + temp);
-					meditation.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_DELTA) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_DELTA);
-					System.out.println("Get delta data: " + temp);
-					delta.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_THETA) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_THETA);
-					System.out.println("Get theta data: " + temp);
-					theta.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_ALPHA1) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_ALPHA1);
-					System.out.println("Get alpha1 data: " + temp);
-					alpha1.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_ALPHA2) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_ALPHA2);
-					System.out.println("Get alpha2 data: " + temp);
-					alpha2.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_BETA1) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_BETA1);
-					System.out.println("Get beta1 data: " + temp);
-					beta1.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_BETA2) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_BETA2);
-					System.out.println("Get beta2 data: " + temp);
-					beta2.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_GAMMA1) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_GAMMA1);
-					System.out.println("Get gamma1 data: " + temp);
-					gamma1.add(temp);
-				}
-				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_GAMMA2) != 0){
-					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_GAMMA2);
-					System.out.println("Get gamma2 data: " + temp);
-					gamma2.add(temp);
-				}
-				
-				
-			}while(packetsRead > 0);	
-		}
-		
-		/**
-		 * 
-		 */
-		
-		
-		
+		i = getState(readings);
 		
 		
 		if( i == 1){
@@ -170,9 +89,188 @@ public class EEGReader {
 		}
 	}
 	
+	/**
+	 * Polls the EEG and collects brain-wave readings returning
+	 * a final arraylist containing the averages of all brain-waves
+	 * for the set amount of time
+	 * 
+	 * @return - An array list 'readings' containing brain-wave information
+	 * as follows:
+	 * readings.get(0) = Attention Average
+	 * readings.get(1) = Meditation Average
+	 * readings.get(2) = Delta Average
+	 * readings.get(3) = Theta Average
+	 * readings.get(4) = Alpha1 Average
+	 * readings.get(5) = Alpha2 Average
+	 * readings.get(6) = Beta1 Average
+	 * readings.get(7) = Beta2 Average
+	 * readings.get(8) = Gamma1 Average
+	 * readings.get(9) = Gamma2 Average
+	 */
+	private ArrayList<Double> getReadings(){	
+		ArrayList<Double> attention = new ArrayList<Double>();
+		ArrayList<Double> meditation = new ArrayList<Double>();
+		ArrayList<Double> delta = new ArrayList<Double>();
+		ArrayList<Double> theta = new ArrayList<Double>();
+		ArrayList<Double> alpha1 = new ArrayList<Double>();
+		ArrayList<Double> alpha2 = new ArrayList<Double>();
+		ArrayList<Double> beta1 = new ArrayList<Double>();
+		ArrayList<Double> beta2 = new ArrayList<Double>();
+		ArrayList<Double> gamma1 = new ArrayList<Double>();
+		ArrayList<Double> gamma2 = new ArrayList<Double>();
+		
+		long startTime = System.currentTimeMillis();
+		int packetsRead = 0;
+		while(getSeconds(System.currentTimeMillis(), startTime) < 3){
+			do{
+				packetsRead = ThinkGear.ReadPackets(connectionId, -1);
+				
+				//Read in all waves
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_ATTENTION) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_ATTENTION);
+					//System.out.println("Get attention data: " + temp);
+					attention.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_MEDITATION) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_MEDITATION);
+					//System.out.println("Get meditation data: " + temp);
+					meditation.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_DELTA) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_DELTA);
+					//System.out.println("Get delta data: " + temp);
+					delta.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_THETA) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_THETA);
+					//System.out.println("Get theta data: " + temp);
+					theta.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_ALPHA1) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_ALPHA1);
+					//System.out.println("Get alpha1 data: " + temp);
+					alpha1.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_ALPHA2) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_ALPHA2);
+					//System.out.println("Get alpha2 data: " + temp);
+					alpha2.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_BETA1) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_BETA1);
+					//System.out.println("Get beta1 data: " + temp);
+					beta1.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_BETA2) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_BETA2);
+					//System.out.println("Get beta2 data: " + temp);
+					beta2.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_GAMMA1) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_GAMMA1);
+					//System.out.println("Get gamma1 data: " + temp);
+					gamma1.add(temp);
+				}
+				if(ThinkGear.GetValueStatus(connectionId, ThinkGear.DATA_GAMMA2) != 0){
+					double temp =  ThinkGear.GetValue(connectionId, ThinkGear.DATA_GAMMA2);
+					//System.out.println("Get gamma2 data: " + temp);
+					gamma2.add(temp);
+				}
+				
+			}while(packetsRead > 0);	
+		}
+		
+		
+		ArrayList<Double> readings = new ArrayList<Double>();
+		readings.add(getAverage(attention)); readings.add(getAverage(meditation));
+		readings.add(getAverage(delta)); readings.add(getAverage(theta));
+		readings.add(getAverage(alpha1)); readings.add(getAverage(alpha2));
+		readings.add(getAverage(beta1)); readings.add(getAverage(beta2));
+		readings.add(getAverage(gamma1)); readings.add(getAverage(gamma2));
+		
+		System.out.println(readings);
+		
+		/**
+		 * 
+		 * Write readings to file
+		 * 
+		 */
+		this.write2File("test.txt", readings);
+		
+		return readings;
+	}
+	
+	/**
+	 * Takes in a list of brainwave readings which have been individually
+	 * averaged, and computes a single reading value which will be attributed
+	 * to a particular cluster of music
+	 * 
+	 * @param readings - List of Brain Wave Readings
+	 * readings.get(0) = Attention Average
+	 * readings.get(1) = Meditation Average
+	 * readings.get(2) = Delta Average
+	 * readings.get(3) = Theta Average
+	 * readings.get(4) = Alpha1 Average
+	 * readings.get(5) = Alpha2 Average
+	 * readings.get(6) = Beta1 Average
+	 * readings.get(7) = Beta2 Average
+	 * readings.get(8) = Gamma1 Average
+	 * readings.get(9) = Gamma2 Average
+	 * 
+	 * @return A state as determined by our classifier in the futre
+	 */
+	private int getState(ArrayList<Double> readings){
+		if(readings.get(0) > 50){
+			return 1;
+		} else {
+			return 2;
+		}
+	}
+	
+	/**
+	 * Returns the average of the values of the array arr
+	 * @param arr - An array of doubles
+	 * @return Average value of arr
+	 */
+	private double getAverage(ArrayList<Double> arr){
+		double sum = 0;
+		for(int i = 0; i < arr.size(); i++){
+			sum += arr.get(i);
+		}
+		return sum/arr.size();
+	}
+	
+	/**
+	 * Writes the data to a specified file indicated via parameter 'fileLoc'
+	 * @param fileLoc
+	 * @param data
+	 */
+	private void write2File(String fileName, ArrayList<Double> data){
+		try {
+            // Assume default encoding.
+            FileWriter fileWriter =
+                new FileWriter(fileName, true);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write(data.toString());
+            bufferedWriter.newLine();
+
+            // Always close files.
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+        }
+	}
+	
 	private static long getSeconds(long msBig, long msLess){
 		return (msBig -  msLess)/1000;
 	}
-
-
 }
